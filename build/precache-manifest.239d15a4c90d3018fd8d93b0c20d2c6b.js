8cf87cea19c3328e317cb1dca0870310961f3251
remaxys <utkarshgitikansh@gmail.com>
2020-04-15 17:03:42 +0530

    9th commit

---
+++ build/precache-manifest.239d15a4c90d3018fd8d93b0c20d2c6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbadb84eb54a6cc18d6baca49123d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c09eced8c66be29bf2e",
+    "url": "/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "5fb72239afca6488e37e",
+    "url": "/static/css/main.cbf55de7.chunk.css"
+  },
+  {
+    "revision": "0c09eced8c66be29bf2e",
+    "url": "/static/js/2.76a765c4.chunk.js"
+  },
+  {
+    "revision": "f6c37ee208461fa046548271a2ed9ef1",
+    "url": "/static/js/2.76a765c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb72239afca6488e37e",
+    "url": "/static/js/main.a9f828c3.chunk.js"
+  },
+  {
+    "revision": "b8f51121efe464bec58c",
+    "url": "/static/js/runtime-main.8b8c8b53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
