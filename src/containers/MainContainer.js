652b314127c358dae343a1fad3693c41598922e0
remaxys <utkarshgitikansh@gmail.com>
2020-04-15 19:34:20 +0530

    15th commiy

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -46,8 +46,8 @@ class MainContainer extends React.Component {
               src="http://free.timeanddate.com/countdown/i78pyxlo/n176/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac6e7f80/cpc000/pc96ceb4/tc66c/fs100/szw320/szh135/tatCountdown%20to%20lockdown/tacfe8a71/tptTime%20since%20Event%20started%20in/tpc000/mat%23StayHomeStaySafe/mac708090/mpc000/iso2020-05-03T23:59:59/bas3/bac6e7f80"
               allowTransparency="true"
               frameborder="0"
-              width="326"
-              height="141"
+              width="auto"
+              height="auto"
             ></iframe>
           </div>
           <div class="p-2 bd-highlight" class="scroll_side">
